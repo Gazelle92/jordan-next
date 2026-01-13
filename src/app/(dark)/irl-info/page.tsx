a1bb3f1703806c569af35a77ab17f170ada1ad39
jordan <hutswing12@gmail.com>
2026-01-13 18:21:33 +0900

    test

--- src/app/(dark)/irl-info/page.tsx
+++ src/app/(dark)/irl-info/page.tsx
@@ -2,6 +2,7 @@ import { HistoryBack } from "@/ui/components/history-back";
 
 import InfraredIrlInfo from "@/ui/svg/infrared_irl_info_2.svg";
 import Logo from "@/ui/svg/logo.svg";
+
 import { Button } from "@/ui/components/button";
 import { Link } from "next-view-transitions";
 import IrlInformation from "@/ui/components/irl-information";
@@ -30,30 +31,20 @@ export default function IrlInfo() {
           <IrlInformation />
           <hr className="border-2" />
           <div className="flex flex-col gap-5">
-            <h2 className="text-[24px] font-black text-center border-b-1 pb-1">
+            <h2 className="text-[24px] font-black text-center">
               프로그램
             </h2>
 
-            <div className="py-[15px] text-[13px] font-semibold whitespace-pre-wrap break-keep ani leading-[1.5]">
-              <h4 className="text-[16px] font-black">01. 2:2 WOMEN'S OPEN STYLE BATTLE</h4>
-              <span>여성 댄서만을 위한 2대2 오픈 스타일 INFRARED BATTLE은 하우스 DJ의 랜덤 플레이 속에서 INFRARED 에너지가 충돌·증폭되며, 64팀 예선부터 변칙 토너먼트까지 흐름으로 GREATNESS를 증명하는 배틀입니다.</span>
+            <div className="py-[15px] text-[13px] font-semibold whitespace-pre-wrap break-keep ani">
+              HOUSE OF GREATNESS는 조던의 정신처럼 멈추지 않는 움직임이 서로를 만나고, 충돌하며, 더 큰 에너지로 확장되는 현장입니다.
+              <br />
+              이곳에서 GREATNESS는 완성된 결과가 아니라, 지금도 생성 중인 흐름입니다.
               <br /><br />
-              <h4 className="text-[16px] font-black">02. CHOREOGRAPHY WORKSHOP</h4>
-              <span>댄서 바다와 스페셜 인스트럭터들과 함께 시그니처 코레오그래피를 배우고 확장하며, 참여자 전원이 하나의 집단 퍼포먼스로 완성하는 현장 중심 워크숍입니다. 파트별 안무 습득과 연결을 거쳐, INFRARED의 에너지를 몸으로 체화하는 무대를 만들어갑니다.</span>
+              여성 댄서들의 주체적인 스텝은 이 흐름의 출발점입니다. 발끝에서 시작된 에너지는 공간을 가로질러 퍼지고, 개인의 움직임은 집단의 리듬이 되어 현장을 채웁니다.
               <br /><br />
-              <h4 className="text-[16px] font-black">03. TALK SESSION</h4>
-              <span>바다·왁씨·MC 킹키가 각자의 위치에서 움직임의 기준과 경험을 공유하는 토크 세션은,코레오그래피와 배틀 씬을 가로지르는 춤의 스펙트럼을 공유하며 참여자들이 각자의 다음 스텝을 그려보는 자리입니다.</span>
-              <br /><br />
-              <h4 className="text-[16px] font-black">04.STYLING WORKSHOP</h4>
-              <span>INFRARED 무드의 티셔츠 커스텀을 통해 댄서 각자의 개성과 자기표현을 확장하는 스타일링 세션입니다. 무대 안팎에서 움직임과 스타일이 하나의 시그니처로 완성되는 경험을 제안합니다.</span>
-
-
-
+              지금, 성수동에서 열리는 HOUSE OF GREATNESS에 들어와 조던이 말하는 움직임의 에너지와 다음 스텝을 향한 순간을 직접 만나보세요.
             </div>
-            <h2 className="text-[24px] font-black text-center border-b-1 border-t-4 pt-1 pb-1">
-              타임테이블
-            </h2>
-            <div className="flex flex-col">
+            <div className="flex flex-col fadeCover-0">
               {programs.map((program) => (
                 <div
                   className={clsx("border-b flex gap-6 px-4 py-3 text-[15px] items-center", jordan.className)}
@@ -66,7 +57,7 @@ export default function IrlInfo() {
               ))}
             </div>
             <Link href="/battles">
-              <Button>HOUSE OF GREATNESS 이벤트 예약하기</Button>
+              <Button>INFRARED BATTLE</Button>
             </Link>
 
           </div>
