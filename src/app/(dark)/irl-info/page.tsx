2122eb12a484623cf3b69216386e19456d698ab1
jordan <hutswing12@gmail.com>
2026-01-13 18:16:04 +0900

    fix2

--- src/app/(dark)/irl-info/page.tsx
+++ src/app/(dark)/irl-info/page.tsx
@@ -36,16 +36,16 @@ export default function IrlInfo() {
 
             <div className="py-[15px] text-[13px] font-semibold whitespace-pre-wrap break-keep ani leading-[1.5]">
               <h4 className="text-[16px] font-black">01. 2:2 WOMEN'S OPEN STYLE BATTLE</h4>
-              여성 댄서만을 위한 2대2 오픈 스타일 INFRARED BATTLE은 하우스 DJ의 랜덤 플레이 속에서 INFRARED 에너지가 충돌·증폭되며, 64팀 예선부터 변칙 토너먼트까지 흐름으로 GREATNESS를 증명하는 배틀입니다.
+              <span>여성 댄서만을 위한 2대2 오픈 스타일 INFRARED BATTLE은 하우스 DJ의 랜덤 플레이 속에서 INFRARED 에너지가 충돌·증폭되며, 64팀 예선부터 변칙 토너먼트까지 흐름으로 GREATNESS를 증명하는 배틀입니다.</span>
               <br /><br />
               <h4 className="text-[16px] font-black">02. CHOREOGRAPHY WORKSHOP</h4>
-              댄서 바다와 스페셜 인스트럭터들과 함께 시그니처 코레오그래피를 배우고 확장하며, 참여자 전원이 하나의 집단 퍼포먼스로 완성하는 현장 중심 워크숍입니다. 파트별 안무 습득과 연결을 거쳐, INFRARED의 에너지를 몸으로 체화하는 무대를 만들어갑니다.
+              <span>댄서 바다와 스페셜 인스트럭터들과 함께 시그니처 코레오그래피를 배우고 확장하며, 참여자 전원이 하나의 집단 퍼포먼스로 완성하는 현장 중심 워크숍입니다. 파트별 안무 습득과 연결을 거쳐, INFRARED의 에너지를 몸으로 체화하는 무대를 만들어갑니다.</span>
               <br /><br />
               <h4 className="text-[16px] font-black">03. TALK SESSION</h4>
-              바다·왁씨·MC 킹키가 각자의 위치에서 움직임의 기준과 경험을 공유하는 토크 세션은,코레오그래피와 배틀 씬을 가로지르는 춤의 스펙트럼을 공유하며 참여자들이 각자의 다음 스텝을 그려보는 자리입니다.
+              <span>바다·왁씨·MC 킹키가 각자의 위치에서 움직임의 기준과 경험을 공유하는 토크 세션은,코레오그래피와 배틀 씬을 가로지르는 춤의 스펙트럼을 공유하며 참여자들이 각자의 다음 스텝을 그려보는 자리입니다.</span>
               <br /><br />
               <h4 className="text-[16px] font-black">04.STYLING WORKSHOP</h4>
-              Infrared 무드의 티셔츠 커스텀을 통해 댄서 각자의 개성과 자기표현을 확장하는 스타일링 세션입니다. 무대 안팎에서 움직임과 스타일이 하나의 시그니처로 완성되는 경험을 제안합니다.
+              <span>INFRARED 무드의 티셔츠 커스텀을 통해 댄서 각자의 개성과 자기표현을 확장하는 스타일링 세션입니다. 무대 안팎에서 움직임과 스타일이 하나의 시그니처로 완성되는 경험을 제안합니다.</span>
 
 
 
